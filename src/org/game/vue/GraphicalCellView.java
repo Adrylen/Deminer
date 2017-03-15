dd1035ba731556fd240a53d5d33742a474c6232d
tsumtsum3003 <sarah.kieffer@isen-lille.fr>
2017-03-15 09:14:18 +0100

    suite affichage graphique

--- src/org/game/vue/GraphicalCellView.java
+++ src/org/game/vue/GraphicalCellView.java
@@ -5,7 +5,9 @@
  */
 package org.game.vue;
 
-import java.awt.Font;
+import java.io.File;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import org.game.models.Case;
 
@@ -16,9 +18,58 @@ import org.game.models.Case;
 public class GraphicalCellView extends JButton{
     
     private Case gameCase;
+    private Icon mine = new ImageIcon("deminer.png");
 
     public GraphicalCellView(Case gameCase){
-        super();
+        super(new StringBuilder().append(gameCase.getSymbol()).toString());
         this.gameCase=gameCase;
+        this.changeIcon();
+    }
+    
+    public void changeIcon(){
+        switch(this.gameCase.getSymbol()){
+            case '#':
+                this.setText("#");
+                break;
+            case '!':
+                this.setText("!");
+                break;
+            case 'x':
+                this.setText("x");
+                break;
+            case '.':
+                this.setText(".");
+                this.setEnabled(false);
+                break;
+            case '?':
+                this.setText("?");
+                break;
+            case '1':
+                this.setText("1");
+                break;
+            case '2':
+                this.setText("2");
+                break;
+            case '3':
+                this.setText("3");
+                break;
+            case '4':
+                this.setText("4");
+                break;
+            case '5':
+                this.setText("5");
+                break;
+            case '6':
+                this.setText("6");
+                break;
+            case '7':
+                this.setText("7");
+                break;
+            case '8':
+                this.setText("8");
+                break;
+            
+                   
+        }
     }
 }
